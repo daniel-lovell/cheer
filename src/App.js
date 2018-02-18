b9fa7f967b6c6ddc2c0e6def78d1df3f9bf544bd
Daniel Lovell <daniel@Daniels-Laptop.local>
2018-02-17 17:08:54 -0700

    add Cheer component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import Cheer from './components/Cheer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <header>Cheer!</header>
+        <main>
+          <button>New Cheer</button>
+          <Cheer name="Politician" description="Pat yourself on the back." />
+        </main>
+      </Fragment>
     );
   }
 }
---
+++ src/components/Cheer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+const Cheer = ({name, description}) => {
+  return (
+    <article>
+      <h1>{name}</h1>
+      <p>{description}</p>
+    </article>
+  )
+}
+
+Cheer.defaultProps = {
+  name: 'Missing Cheer Name',
+  description: 'Missing Cheer Description'
+}
+
+Cheer.propTypes = {
+  name: propTypes.string.isRequired,
+  description: propTypes.string.isRequired
+}
+
+export default Cheer;
